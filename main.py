d94c06491ee1a5e211f98b68815fa7ccff998500
Rohan Prinja <rohan.prinja@gmail.com>
2014-09-30 20:34:59 +0530

    remove useless valence fn, move tests, fix set change bug

--- main.py
+++ main.py
@@ -17,9 +17,6 @@ class Node(object):
         if n is not None:
             self.neighbours.add(n)
 
-    def valence(self):
-        return len(self.neighbours)
-
 im = Image.open(sys.argv[1])
 w, h = im.size
 # print im.size, im.mode
@@ -49,73 +46,6 @@ for row in xrange(h):
                 if x != 0 or y != 0:
                     neighbour = get_node(col+x, row+y, im)
                     n.make_conn(neighbour)
-
-# convert rgb to yuv
-def rgb2yuv(r,g,b):
-    r1 = r / 255.0
-    g1 = g / 255.0
-    b1 = b / 255.0
-    y = (0.299 * r1) + (0.587 * g1) + (0.114 * b1)
-    u = 0.492 * (b1 - y)
-    v = 0.877 * (r1 - y)
-    return (y, u, v)
-
-# compare YUV values of two pixels, return
-# True if they are different, else False
-def pixels_are_dissimilar(pix1, pix2):
-    r1, g1, b1 = pix1
-    r2, g2, b2 = pix2
-    y1, u1, v1 = rgb2yuv(r1, g1, b1)
-    y2, u2, v2 = rgb2yuv(r2, g2, b2)
-    ydiff = abs(y1 - y2) > 48.0/255
-    udiff = abs(u1 - u2) > 7.0/255
-    vdiff = abs(v1 - v2) > 6.0/255
-    return ydiff or udiff or vdiff
-
-# remove dissimilar edges by yuv metric
-for x in xrange(0, w):
-    for y in xrange(0, h):
-        n = get_node(x, y, im)
-        for ne in n.neighbours:
-            if pixels_are_dissimilar(n.rgb, ne.rgb):
-                n.neighbours.remove(ne)
-                ne.neighbours.remove(n)
-
-# apply heuristics to make graph planar
-for x in xrange(0, w-1):
-    for y in xrange(0, h-1):
-        n = get_node(x, y, im)
-        right = get_node(x+1, y, im) # node to the right of the curr node
-        down = get_node(x, y+1, im) # node directly below the curr node
-        rightdown = get_node(x+1, y+1, im) # node directly below and to the right of the curr node
-
-        # edges
-        self_to_right = right in n.neighbours
-        self_to_down = down in n.neighbours
-        right_to_rightdown = right in rightdown.neighbours
-        down_to_rightdown = down in rightdown.neighbours
-        # diagonals
-        diag1 = rightdown in n.neighbours
-        diag2 = down in right.neighbours
-
-        # check if fully connected
-        vert_and_horiz_edges = self_to_right and self_to_down and right_to_rightdown and down_to_rightdown
-        no_vert_horiz_edges = not (self_to_right or self_to_down or right_to_rightdown or down_to_rightdown)
-        both_diagonals = diag1 and diag2
-
-        fully_connected = vert_and_horiz_edges and both_diagonals # all 6 connections are present
-        only_diagonals = no_vert_horiz_edges and both_diagonals # only the diagonals are present
-
-        if fully_connected:
-            # remove both diagonals
-            n.neighbours.remove(rightdown)
-            rightdown.neighbours.remove(n)
-            right.neighbours.remove(down)
-            down.neighbours.remove(right)
-
-        if only_diagonals:
-            # apply heuristics - TODO
-            pass
 
 '''tests'''
 
@@ -155,3 +85,72 @@ def number_of_neighbours_is_correct(im):
 node_corresponds_to_image(im)
 number_of_neighbours_is_correct(im)
 neighbours_are_mutual(im)
+
+'''tests over'''
+
+# convert rgb to yuv
+def rgb2yuv(r,g,b):
+    r1 = r / 255.0
+    g1 = g / 255.0
+    b1 = b / 255.0
+    y = (0.299 * r1) + (0.587 * g1) + (0.114 * b1)
+    u = 0.492 * (b1 - y)
+    v = 0.877 * (r1 - y)
+    return (y, u, v)
+
+# compare YUV values of two pixels, return
+# True if they are different, else False
+def pixels_are_dissimilar(pix1, pix2):
+    r1, g1, b1 = pix1
+    r2, g2, b2 = pix2
+    y1, u1, v1 = rgb2yuv(r1, g1, b1)
+    y2, u2, v2 = rgb2yuv(r2, g2, b2)
+    ydiff = abs(y1 - y2) > 48.0/255
+    udiff = abs(u1 - u2) > 7.0/255
+    vdiff = abs(v1 - v2) > 6.0/255
+    return ydiff or udiff or vdiff
+
+# remove dissimilar edges by yuv metric
+for x in xrange(0, w):
+    for y in xrange(0, h):
+        n = get_node(x, y, im)
+        neighbours_to_remove = [ne for ne in n.neighbours if pixels_are_dissimilar(n.rgb, ne.rgb)]
+        for ne in neighbours_to_remove:
+            n.neighbours.remove(ne)
+            ne.neighbours.remove(n)
+
+# apply heuristics to make graph planar
+for x in xrange(0, w-1):
+    for y in xrange(0, h-1):
+        n = get_node(x, y, im)
+        right = get_node(x+1, y, im) # node to the right of the curr node
+        down = get_node(x, y+1, im) # node directly below the curr node
+        rightdown = get_node(x+1, y+1, im) # node directly below and to the right of the curr node
+
+        # edges
+        self_to_right = right in n.neighbours
+        self_to_down = down in n.neighbours
+        right_to_rightdown = right in rightdown.neighbours
+        down_to_rightdown = down in rightdown.neighbours
+        # diagonals
+        diag1 = rightdown in n.neighbours
+        diag2 = down in right.neighbours
+
+        # check if fully connected
+        vert_and_horiz_edges = self_to_right and self_to_down and right_to_rightdown and down_to_rightdown
+        no_vert_horiz_edges = not (self_to_right or self_to_down or right_to_rightdown or down_to_rightdown)
+        both_diagonals = diag1 and diag2
+
+        fully_connected = vert_and_horiz_edges and both_diagonals # all 6 connections are present
+        only_diagonals = no_vert_horiz_edges and both_diagonals # only the diagonals are present
+
+        if fully_connected:
+            # remove both diagonals
+            n.neighbours.remove(rightdown)
+            rightdown.neighbours.remove(n)
+            right.neighbours.remove(down)
+            down.neighbours.remove(right)
+
+        if only_diagonals:
+            # apply heuristics - TODO
+            pass
